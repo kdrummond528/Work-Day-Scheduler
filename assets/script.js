cf51f69f66dfd5731eba780df8ef70fff9b31e67
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 09:25:58 -0800

    modified js

--- assets/script.js
+++ assets/script.js
@@ -7,5 +7,16 @@ function currentTime() {
     // get the hours from current time
     var currentHour = moment().hour();
 
+    // for loop for each of the hour color blocks if past present future
+    $(".time-block").each(function () {
+        var hourBlock
+
+    })
 
 }
+
+// save button event listener
+
+// save user input to local storage when user clicks save button
+
+// get from local storage 
