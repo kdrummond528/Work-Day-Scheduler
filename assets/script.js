7118f189c4abaa684f82e9640939d2c9d00ec79e
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 09:47:49 -0800

    modified js and html

--- assets/script.js
+++ assets/script.js
@@ -9,24 +9,24 @@ function currentTime() {
 
     // for loop for each of the hour color blocks if past present future
     $(".time-block").each(function () {
-        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+        var hourBlock = parseInt($(this).attr("id")[1]);
 
         // if the hour row is less than the current hour make row gray for past
         if (hourBlock < currentHour) {
             $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
+            // $(this).removeClass("present");
+            // $(this).removeClass("future");
         }
         // if the hour row is equal to the current hour make row red for present
         else if (hourBlock === currentHour) {
-            $(this).removeClass("past");
+            // $(this).removeClass("past");
             $(this).addClass("present");
-            $(this).removeClass("future");
+            // $(this).removeClass("future");
         }
         // if the hour row is greater than the current hour make row green for future
         else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
+            // $(this).removeClass("past");
+            // $(this).removeClass("present");
             $(this).addClass("future");
         }
     })
