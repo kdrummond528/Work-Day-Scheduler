c64b82842392bb25d5cd757ec64041d4d26a90c7
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 09:19:42 -0800

    modified js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
-// show todays date in jumbotron
+// show todays date in the header/jumbotron
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+// function to get the current time
+function currentTime() {
+    // get the hours from current time
+    var currentHour = moment().hour();
+
+
+}
